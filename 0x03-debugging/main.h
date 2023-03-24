3c7e56d90ed9a017f9916539dd63e1fcce0ef8eb
OlatunjiMariamm <hardeola04@gmail.com>
2023-03-23 18:55:15 -0700

    task 3

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+int_pucher(char c);
+
+int_0-main.c, main.h
+
+int_1-main.c
+
+int_2-largest_number.c, main.h
+
+int_3-print_remaining_days.c, main.h
+
+#endif
